d277124561e1bd74a109a4369a730abacb6dcf84
irvin <irvin@aufs.io>
2021-10-18 13:07:22 +0800

    refactor walk level fn

--- mantaray/tree_walker.go
+++ mantaray/tree_walker.go
@@ -5,6 +5,7 @@ import (
 	"container/list"
 	"context"
 	"errors"
+	"math"
 )
 
 type WalkLevelFunc func(nodeType int, path, prefix, hash []byte) error
@@ -15,15 +16,60 @@ const (
 )
 
 var (
-	emptyPath    = []byte{}
 	errMaxHeight = errors.New("reach maximum directory level")
+	errAlreadyEntered = errors.New("already entered the directory")
 )
 
-type nodeTag struct {
-	*Node
-	path    []byte
-	subPath []byte
-	level   uint
+func copyPath(src []byte) []byte {
+	p := make([]byte, len(src), len(src))
+	copy(p, src)
+	return p
+}
+
+func walkDeepFirst(ctx context.Context, l Loader, n *Node, path, prefix []byte, walker WalkLevelFunc) error {
+	if len(n.forks) == 0 {
+		if err := n.load(ctx, l); err != nil {
+			return err
+		}
+	}
+
+	nextPath := make([]byte, len(path), len(path)+len(prefix))
+	copy(nextPath, path)
+
+	for i := 0; i < len(prefix); i++ {
+		nextPath = append(nextPath, prefix[i])
+		if prefix[i] == byte(PathSeparator) {
+			if err := walker(Directory, copyPath(nextPath), []byte{}, nil); err != nil {
+				return err
+			}
+		}
+	}
+
+	if n.IsValueType() {
+		if len(nextPath) != 0 && nextPath[len(nextPath)-1] != byte(PathSeparator) {
+			afterSlash := bytes.LastIndexByte(nextPath, byte(PathSeparator))
+			var curPath, curFile []byte
+			if afterSlash != -1 {
+				curPath = nextPath[:afterSlash+1]
+				curFile = nextPath[afterSlash+1:]
+			} else {
+				curPath = []byte{}
+				curFile = nextPath
+			}
+			if err := walker(File, copyPath(curPath), copyPath(curFile), n.Reference()); err != nil {
+				return err
+			}
+		}
+	}
+
+	for _, v := range n.forks {
+		err := walkDeepFirst(ctx, l, v.Node, nextPath, v.prefix, walker)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (n *Node) lookupClosest(ctx context.Context, path []byte, l Loader) (*Node, []byte, error) {
@@ -48,206 +94,45 @@ func (n *Node) lookupClosest(ctx context.Context, path []byte, l Loader) (*Node,
 	if len(c) == len(f.prefix) {
 		return f.Node.lookupClosest(ctx, path[len(c):], l)
 	}
-	return n, path, nil
+	return f.Node, f.prefix[len(c):], errAlreadyEntered
 }
 
-func (n *Node) WalkLevel(ctx context.Context, root []byte, l Loader, level uint, walker WalkLevelFunc) error {
-	node, remain, err := n.lookupClosest(ctx, root, l)
-	if err != nil {
-		return err
+type nodeTag struct {
+	*Node
+	level uint
+	path []byte
+	prefix []byte
+}
+
+func walkBreathFirst(ctx context.Context, l Loader, n *Node, path []byte, level uint, walker WalkLevelFunc) error {
+	p, remain, err := n.lookupClosest(ctx, path, l)
+	root := &nodeTag{
+		Node: p,
+		path: []byte{},
+		prefix: []byte{},
 	}
 
-	// path stack
-	s := make([][]byte, 0)
+	if len(remain) == 0 {
+		root.level++
+	}
 
-	if len(root) != 0 {
-		lastSlash := bytes.LastIndexByte(root, byte(PathSeparator))
-		if lastSlash != -1 {
-			if len(remain) > 0 && remain[0] == byte(PathSeparator) {
-				root = root[lastSlash:]
-			} else {
-				root = root[lastSlash+1:]
-			}
+	if err != nil {
+		if !errors.Is(err, errAlreadyEntered) {
+			return err
 		}
-
-		root = bytes.TrimSuffix(root, remain)
-		remain = append(remain, byte(PathSeparator))
+		root.prefix = remain
+		root.level++
 	}
 
 	q := list.New()
-	q.PushBack(&nodeTag{Node: node, path: root, subPath: emptyPath})
+	q.PushBack(root)
 
-	var (
-		nextLevel     func(n *Node, path, sub, prefix []byte, cur uint) error
-		readDirectory func(fn WalkLevelFunc, start int, path []byte, pCur *uint) (int, error)
-	)
-
-	readDirectory = func(fn WalkLevelFunc, start int, path []byte, pCur *uint) (int, error) {
-		i := start
-		for j := i; j < len(path); j++ {
-			if path[j] == byte(PathSeparator) {
-				copyPath := make([]byte, j+1)
-				copy(copyPath, path[:j+1])
-				if err := walker(Directory, copyPath, []byte{}, nil); err != nil {
-					return j, err
-				}
-				i = j + 1
-				*pCur++
-			}
-			if *pCur > level {
-				return i, errMaxHeight
-			}
-		}
-		return i, nil
-	}
-
-	nextLevel = func(n *Node, path, sub, prefix []byte, cur uint) error {
-		if cur > level {
-			return nil
-		}
-
-		next, err := n.LookupNode(ctx, prefix, l)
-		if err != nil {
-			return err
-		}
-
-		prefix = append(sub, prefix...)
-
-		if prefix[0] == byte(PathSeparator) {
-			path = append(path, prefix[0])
-			prefix = prefix[1:]
-		}
-
-		path = bytes.TrimPrefix(path, []byte{byte(PathSeparator)})
-
-		lastPathSlash := bytes.LastIndexByte(path, byte(PathSeparator)) + 1
-		lastPrefixSlash := bytes.LastIndexByte(prefix, byte(PathSeparator))
-
-		if next.IsValueType() && lastPrefixSlash < len(prefix)-1 {
-			curPath := path
-			curPrefix := prefix
-
-			if lastPrefixSlash != -1 {
-				curPath = append(curPath, prefix...)
-				idx, err := readDirectory(walker, lastPathSlash, curPath, &cur)
-				if err != nil {
-					if errors.Is(err, errMaxHeight) {
-						return nil
-					}
-					return err
-				}
-				lastPathSlash = idx
-				curPrefix = curPath[idx:]
-				curPath = curPath[:idx]
-			}
-
-			if err := walker(File, curPath, curPrefix, next.Reference()); err != nil {
-				return err
-			}
-		}
-
-		if !next.IsEdgeType() {
-			return nil
-		}
-
-		path = append(path, prefix...)
-
-		if lastPrefixSlash != -1 {
-			idx, err := readDirectory(walker, lastPathSlash, path, &cur)
-			if err != nil {
-				if !errors.Is(err, errMaxHeight) {
-					return err
-				}
-			}
-			lastPathSlash = idx
-		}
-
-		for _, fork := range next.forks {
-			if fork.IsValueType() {
-				tempCur := cur
-				if fork.IsWithPathSeparatorType() {
-					if err := nextLevel(next, path, emptyPath, fork.prefix, tempCur); err != nil {
-						return err
-					}
-					continue
-				} else {
-					lastForkSlash := bytes.LastIndexByte(fork.prefix, byte(PathSeparator))
-					curPath := make([]byte, len(path))
-					copy(curPath, path)
-					curPrefix := make([]byte, len(prefix))
-					copy(curPrefix, prefix)
-					curPrefix = curPrefix[lastPrefixSlash+1:]
-					if lastForkSlash != -1 {
-						curPath = append(curPath, fork.prefix[:lastForkSlash+1]...)
-						idx, err := readDirectory(walker, lastPathSlash, curPath, &tempCur)
-						if err != nil {
-							if errors.Is(err, errMaxHeight) {
-								continue
-							}
-							return err
-						}
-						curPath = curPath[:idx]
-						curPrefix = fork.prefix[lastForkSlash+1:]
-					} else {
-						curPath = bytes.TrimSuffix(curPath, curPrefix)
-						curPrefix = append(curPrefix, fork.prefix...)
-					}
-					if tempCur <= level {
-						if err := walker(File, curPath, curPrefix, fork.Reference()); err != nil {
-							return err
-						}
-					}
-				}
-			}
-
-			if fork.IsEdgeType() {
-				var curPath, subPath []byte
-				dirSlash := bytes.LastIndexByte(fork.prefix, byte(PathSeparator))
-				tempCur := cur
-				if dirSlash != -1 {
-					copyPath := make([]byte, len(path)+dirSlash+1)
-					copy(copyPath, path)
-					copy(copyPath[len(path):], fork.prefix[:dirSlash+1])
-					idx, err := readDirectory(walker, lastPathSlash, copyPath, &tempCur)
-					if err != nil {
-						if !errors.Is(err, errMaxHeight) {
-							return err
-						}
-					}
-					curPath = copyPath[:idx]
-					subPath = fork.prefix[dirSlash+1:]
-					if bytes.HasSuffix(curPath, []byte{byte(PathSeparator)}) {
-						tempCur++
-					}
-				} else {
-					slashIndex := bytes.LastIndexByte(path, byte(PathSeparator)) + 1
-					if slashIndex != 0 {
-						curPath = path[:slashIndex]
-						subPath = make([]byte, len(path)+len(fork.prefix)-slashIndex)
-						copy(subPath, path[slashIndex:])
-						copy(subPath[len(path)-slashIndex:], fork.prefix)
-					} else {
-						curPath = path
-						subPath = make([]byte, len(fork.prefix))
-						copy(subPath, fork.prefix)
-					}
-					if slashIndex != 0 && slashIndex != lastPathSlash {
-						tempCur++
-					}
-				}
-				if tempCur <= level {
-					q.PushBack(&nodeTag{Node: fork.Node, path: curPath, subPath: subPath, level: tempCur})
-				}
-			}
-		}
-
-		return nil
-	}
-
+pop:
 	for q.Len() > 0 {
 		e := q.Front()
-		q.Remove(e)
 		t := e.Value.(*nodeTag)
+
+		q.Remove(e)
 
 		if t.forks == nil {
 			if err := t.load(ctx, l); err != nil {
@@ -255,38 +140,56 @@ func (n *Node) WalkLevel(ctx context.Context, root []byte, l Loader, level uint,
 			}
 		}
 
-		for _, b := range t.forks {
-			s = append(s, b.prefix)
-		}
+		nextPath := make([]byte, len(t.path), len(t.path)+len(t.prefix))
+		copy(nextPath, t.path)
 
-		l := t.level
-		path := append(t.path, t.subPath...)
-
-		lastPathSlash := bytes.LastIndexByte(path, byte(PathSeparator)) + 1
-
-		idx, err := readDirectory(walker, lastPathSlash, path, &l)
-		if err != nil {
-			if !errors.Is(err, errMaxHeight) {
-				return err
-			}
-		}
-
-		end := len(s)
-		for len(s) != 0 {
-			prefix := s[end-1]
-			couldNext := true
-			if len(remain) > 0 && t.level == 0 && !bytes.HasPrefix(prefix, remain) {
-				couldNext = false
-			}
-			if couldNext {
-				if err := nextLevel(t.Node, path[:idx], path[idx:], prefix, t.level); err != nil {
+		for i := 0; i < len(t.prefix); i++ {
+			nextPath = append(nextPath, t.prefix[i])
+			if t.prefix[i] == byte(PathSeparator) {
+				t.level++
+				if err := walker(Directory, copyPath(nextPath), []byte{}, nil); err != nil {
 					return err
 				}
 			}
-			end--
-			s = s[:end]
+			if t.level > level {
+				continue pop
+			}
+		}
+
+		if t.IsValueType() {
+			if len(nextPath) != 0 && nextPath[len(nextPath)-1] != byte(PathSeparator) {
+				afterSlash := bytes.LastIndexByte(nextPath, byte(PathSeparator))
+				var curPath, curFile []byte
+				if afterSlash != -1 {
+					curPath = nextPath[:afterSlash+1]
+					curFile = nextPath[afterSlash+1:]
+				} else {
+					curPath = []byte{}
+					curFile = nextPath
+				}
+				if err := walker(File, copyPath(curPath), copyPath(curFile), t.Reference()); err != nil {
+					return err
+				}
+			}
+		}
+
+		for _, v := range t.forks {
+			q.PushBack(&nodeTag{
+				Node: v.Node,
+				path: nextPath,
+				level: t.level,
+				prefix: v.prefix,
+			})
 		}
 	}
 
 	return nil
+}
+
+func (n *Node) WalkLevel(ctx context.Context, root []byte, l Loader, level uint, walker WalkLevelFunc) error {
+	if len(root) == 0 && level == math.MaxUint64 {
+		return walkDeepFirst(ctx, l, n, root, []byte{}, walker)
+	}
+
+	return walkBreathFirst(ctx, l, n, root, level, walker)
 }
--- mantaray/tree_walker_test.go
+++ mantaray/tree_walker_test.go
@@ -8,7 +8,7 @@ import (
 	"testing"
 )
 
-func TestWalkLevel(t *testing.T) {
+func TestWalkLevelAtExpandAll(t *testing.T) {
 	for _, tc := range []struct {
 		name     string
 		toAdd    [][]byte
@@ -16,6 +16,15 @@ func TestWalkLevel(t *testing.T) {
 	}{
 		{
 			name: "simple",
+			toAdd: [][]byte{
+				[]byte("empty"),
+			},
+			expected: [][]byte{
+				[]byte("empty"),
+			},
+		},
+		{
+			name: "simple-dir",
 			toAdd: [][]byte{
 				[]byte("index.html"),
 				[]byte("img/test/"),
@@ -34,31 +43,121 @@ func TestWalkLevel(t *testing.T) {
 			},
 			expected: [][]byte{
 				[]byte("index.html"),
-				[]byte("img"),
-				[]byte("img/test"),
+				[]byte("img/"),
+				[]byte("img/test/"),
 				[]byte("img/test/oho.png"),
-				[]byte("img/test/old"),
+				[]byte("img/test/old/"),
 				[]byte("img/test/old/test.png"),
-				[]byte("img/test/olds"),
+				[]byte("img/test/olds/"),
 				[]byte("img/test/olds/person.jpg"),
-				[]byte("img/test/ow"),
-				[]byte("img/test/ow/secret"),
+				[]byte("img/test/ow/"),
+				[]byte("img/test/ow/secret/"),
 				[]byte("img/test/ow/secret/.empty"),
 				[]byte("robots.txt"),
-				[]byte("robot"),
+				[]byte("robot/"),
 				[]byte("robot/baidu.com"),
-				[]byte("robot/google"),
+				[]byte("robot/google/"),
 				[]byte("robot/google/robots.txt"),
-				[]byte("robot/baidu"),
+				[]byte("robot/baidu/"),
 				[]byte("robot/baidu/robots.txt"),
-				[]byte("src"),
+				[]byte("src/"),
 				[]byte("src/logo.gif"),
-				[]byte("src/default"),
+				[]byte("src/default/"),
 				[]byte("src/default/check.jpg"),
-				[]byte("src/defaults"),
-				[]byte("src/defaults/1"),
+				[]byte("src/defaults/"),
+				[]byte("src/defaults/1/"),
 				[]byte("src/defaults/1/apple.png"),
 				[]byte("src/defaults/1/apple.png.bak"),
+			},
+		},
+		{
+			name: "no-direct-dir",
+			toAdd: [][]byte{
+				[]byte("dir/"),
+				[]byte("dir/aufs/"),
+				[]byte("dir/aufs/app"),
+				[]byte("dir/aufs.old/"),
+				[]byte("dir/aufs.old/app"),
+				[]byte("dir/aux"),
+				[]byte("dir/video.tar"),
+				[]byte("dir/video/"),
+				[]byte("dir/video/file"),
+			},
+			expected: [][]byte{
+				[]byte("dir/"),
+				[]byte("dir/aufs/"),
+				[]byte("dir/aufs/app"),
+				[]byte("dir/aufs.old/"),
+				[]byte("dir/aufs.old/app"),
+				[]byte("dir/aux"),
+				[]byte("dir/video.tar"),
+				[]byte("dir/video/"),
+				[]byte("dir/video/file"),
+			},
+		},
+		{
+			name: "complex-dir",
+			toAdd: [][]byte{
+				[]byte("dir1/"),
+				[]byte("dir1/di/"),
+				[]byte("dir1/dx.txt"),
+				[]byte("dir1/dx1.txt"),
+				[]byte("dir1/di/a/"),
+				[]byte("dir1/di/a/b/"),
+				[]byte("dir1/di/a/b/x/"),
+				[]byte("dir1/di/a/b/x.txt"),
+				[]byte("dir1/di/a/b/x/cv.txt"),
+				[]byte("dir1/di/a/c/"),
+				[]byte("dir1/di/a/caaa.txt"),
+				[]byte("dir1/di/a/c/aaa.txt"),
+				[]byte("dir1/di/ab/"),
+				[]byte("dir1/di/ab/c.txt"),
+				[]byte("dir2/"),
+				[]byte("dir2/abc/"),
+				[]byte("dir2/abcde/"),
+				[]byte("dir2/abc/de/"),
+				[]byte("dir2/abc/de.txt"),
+				[]byte("dir2/abc/de1.txt"),
+				[]byte("dir2/abc/de/1.txt"),
+				[]byte("dir2/abc/de/mm/"),
+				[]byte("dir2/abc/de/mm/n.txt"),
+				[]byte("dir2/abcde/1.txt"),
+				[]byte("dir3/"),
+				[]byte("dir3/1.txt"),
+				[]byte("dir3/12.txt"),
+				[]byte("dir3/2.txt"),
+				[]byte("dir3/222.txt"),
+			},
+			expected: [][]byte{
+				[]byte("dir1/"),
+				[]byte("dir1/di/"),
+				[]byte("dir1/dx.txt"),
+				[]byte("dir1/dx1.txt"),
+				[]byte("dir1/di/a/"),
+				[]byte("dir1/di/a/b/"),
+				[]byte("dir1/di/a/b/x/"),
+				[]byte("dir1/di/a/b/x.txt"),
+				[]byte("dir1/di/a/b/x/cv.txt"),
+				[]byte("dir1/di/a/c/"),
+				[]byte("dir1/di/a/caaa.txt"),
+				[]byte("dir1/di/a/c/aaa.txt"),
+				[]byte("dir1/di/ab/"),
+				[]byte("dir1/di/ab/c.txt"),
+				[]byte("dir2/"),
+				[]byte("dir2/abc/"),
+				[]byte("dir2/abcde/"),
+				[]byte("dir2/abc/de/"),
+				[]byte("dir2/abc/de.txt"),
+				[]byte("dir2/abc/de1.txt"),
+				[]byte("dir2/abc/de/1.txt"),
+				[]byte("dir2/abc/de/mm/"),
+				[]byte("dir2/abc/de/mm/n.txt"),
+				[]byte("dir2/abcde/1.txt"),
+				[]byte("dir3/"),
+				[]byte("dir3/1.txt"),
+				[]byte("dir3/12.txt"),
+				[]byte("dir3/2.txt"),
+				[]byte("dir3/222.txt"),
 			},
 		},
 	} {
@@ -82,11 +181,12 @@ func TestWalkLevel(t *testing.T) {
 
 				pathFound := false
 
-				fullPath := make([]byte, len(path)+len(prefix))
+				fullPath := make([]byte, len(path))
 				copy(fullPath, path)
-				copy(fullPath[len(path):], prefix)
 
-				fullPath = bytes.TrimSuffix(fullPath, []byte{byte(PathSeparator)})
+				if nodeType == File {
+					fullPath = append(fullPath, prefix...)
+				}
 
 				for i := 0; i < len(tc.expected); i++ {
 					c := tc.expected[i]
@@ -112,6 +212,191 @@ func TestWalkLevel(t *testing.T) {
 				t.Errorf("expected %d nodes, got %d", len(tc.expected), walkedCount)
 			}
 
+		})
+	}
+}
+
+func TestWalkLevelAtCollapse(t *testing.T) {
+	for _, tc := range []struct{
+		name     string
+		level    int
+		root     []byte
+		toAdd    [][]byte
+		expected [][]byte
+	}{
+		{
+			name: "simple",
+			level: 1,
+			root: []byte{},
+			toAdd: [][]byte{
+				[]byte("dir1/"),
+				[]byte("dir2/"),
+				[]byte("dir3/"),
+			},
+			expected: [][]byte{
+				[]byte("dir1/"),
+				[]byte("dir2/"),
+				[]byte("dir3/"),
+			},
+		},
+		{
+			name: "dive-into-dir",
+			level: 2,
+			root: []byte{},
+			toAdd: [][]byte{
+				[]byte("dir1/"),
+				[]byte("dir2/"),
+				[]byte("dir3/"),
+				[]byte("dir1/di/file"),
+				[]byte("dir1/dx.txt"),
+			},
+			expected: [][]byte{
+				[]byte("dir1/"),
+				[]byte("dir2/"),
+				[]byte("dir3/"),
+				[]byte("dir1/di/"),
+				[]byte("dir1/dx.txt"),
+			},
+		},
+		{
+			name: "file-and-dir-share-prefix",
+			level: 1,
+			root: []byte("dir2/"),
+			toAdd: [][]byte{
+				[]byte("dir1/"),
+				[]byte("dir1/di/file"),
+				[]byte("dir1/dx.txt"),
+				[]byte("dir2/direct.old"),
+				[]byte("dir2/direct/file"),
+			},
+			expected: [][]byte{
+				[]byte("direct.old"),
+				[]byte("direct/"),
+			},
+		},
+		{
+			name: "dir-with-same-prefix",
+			level: 1,
+			root: []byte{},
+			toAdd: [][]byte{
+				[]byte("abc/file"),
+				[]byte("abcde/file"),
+				[]byte("abx/file"),
+				[]byte("a/file"),
+				[]byte("b/file"),
+				[]byte("bc/file"),
+			},
+			expected: [][]byte{
+				[]byte("abc/"),
+				[]byte("abcde/"),
+				[]byte("abx/"),
+				[]byte("a/"),
+				[]byte("b/"),
+				[]byte("bc/"),
+			},
+		},
+		{
+			name: "no-direct-dir",
+			level: 1,
+			root: []byte("dir/"),
+			toAdd: [][]byte{
+				[]byte("dir/"),
+				[]byte("dir/aufs/app"),
+				[]byte("dir/aufs.old/app"),
+				[]byte("dir/aux"),
+				[]byte("dir/video.tar"),
+				[]byte("dir/video/file"),
+			},
+			expected: [][]byte{
+				[]byte("aufs/"),
+				[]byte("aufs.old/"),
+				[]byte("aux"),
+				[]byte("video.tar"),
+				[]byte("video/"),
+			},
+		},
+		{
+			name: "deep-dir-share-prefix",
+			level: 3,
+			root: []byte("dir/"),
+			toAdd: [][]byte{
+				[]byte("dir/a/"),
+				[]byte("dir/a/b/"),
+				[]byte("dir/a/b/c/"),
+				[]byte("dir/a/b/c/d/"),
+				[]byte("dir/a/b/c/d/e/"),
+				[]byte("dir/a/b/c/d/e/file"),
+				[]byte("dir/a/b/cde/"),
+				[]byte("dir/a/b/cde/file"),
+				[]byte("dir/abc/"),
+				[]byte("dir/abc/de/"),
+				[]byte("dir/abc/de/file"),
+				[]byte("dir/abcde/"),
+				[]byte("dir/abcde/file"),
+			},
+			expected: [][]byte{
+				[]byte("a/"),
+				[]byte("a/b/"),
+				[]byte("a/b/c/"),
+				[]byte("a/b/cde/"),
+				[]byte("abc/"),
+				[]byte("abc/de/"),
+				[]byte("abc/de/file"),
+				[]byte("abcde/"),
+				[]byte("abcde/file"),
+			},
+		},
+	}{
+		ctx := context.Background()
+		t.Run(tc.name, func(t *testing.T) {
+			n := New()
+
+			for i := 0; i < len(tc.toAdd); i++ {
+				c := tc.toAdd[i]
+				e := append(make([]byte, 32-len(c)), c...)
+				err := n.Add(ctx, c, e, nil, nil)
+				if err != nil {
+					t.Fatalf("expected no error, got %v", err)
+				}
+			}
+
+			walkedCount := 0
+
+			walker := func(nodeType int, path, prefix, hash []byte) error {
+				walkedCount++
+
+				pathFound := false
+
+				fullPath := make([]byte, len(path))
+				copy(fullPath, path)
+
+				if nodeType == File {
+					fullPath = append(fullPath, prefix...)
+				}
+
+				for i := 0; i < len(tc.expected); i++ {
+					c := tc.expected[i]
+					if bytes.Equal(fullPath, c) {
+						pathFound = true
+						break
+					}
+				}
+
+				if !pathFound {
+					return fmt.Errorf("walkFn returned unknown path: %s", fullPath)
+				}
+
+				return nil
+			}
+			// Expect no errors.
+			err := n.WalkLevel(ctx, tc.root, nil, uint(tc.level), walker)
+			if err != nil {
+				t.Fatalf("no error expected, found: %s", err)
+			}
+
+			if len(tc.expected) != walkedCount {
+				t.Errorf("expected %d nodes, got %d", len(tc.expected), walkedCount)
+			}
 		})
 	}
 }
